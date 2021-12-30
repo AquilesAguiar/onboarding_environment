20142d2e3ddbbaaa372d32bb93342dfe76048ca8
Aquiles Aguiar <67012626+AquilesAguiar@users.noreply.github.com>
2021-12-30 11:52:38 -0300

    refactoring redis cache

--- phoenix/app/cad_products_phoenix/lib/cad_products_phoenix_web/controllers/register_controller.ex
+++ phoenix/app/cad_products_phoenix/lib/cad_products_phoenix_web/controllers/register_controller.ex
@@ -1,13 +1,11 @@
 defmodule CadProductsPhoenixWeb.RegisterController do
   use CadProductsPhoenixWeb, :controller
 
-  alias CadProductsPhoenix.Management
-  alias CadProductsPhoenix.Management.Register
   alias CadProductsPhoenixWeb.Services.Product
 
   action_fallback CadProductsPhoenixWeb.FallbackController
 
-  plug :search_product when action in [:show, :update, :delete]
+  # plug :search_product when action in [:show, :update, :delete]
 
   def index(conn, _params) do
     case Product.fetch_all() do
@@ -21,30 +19,30 @@ defmodule CadProductsPhoenixWeb.RegisterController do
   end
 
   def show(conn, _) do
-    render(conn, "show.json", register: conn.assigns[:register])
+    Product.show(conn.params["id"])
   end
 
   def update(conn, params) do
-    Product.update(conn.assigns[:register], params)
+    Product.update(conn.params["id"], params)
   end
 
   def delete(conn, _) do
-    Product.delete(conn.assigns[:register])
+    Product.delete(conn.params["id"])
     {:ok, :no_content}
   end
 
   # Plug for get products
-  defp search_product(conn, _) do
-    id = conn.params["id"]
-    register = Management.get_register(id)
-    if register do
-      assign(conn, :register, register)
-    else
-      conn
-      |> put_status(:not_found)
-      |> json(%{error: "Product with #{id} not found"})
-      |> halt()
-    end
-  end
+  # defp search_product(conn, _) do
+  #   id = conn.params["id"]
+  #   register = Management.get_register(id)
+  #   if register do
+  #     assign(conn, :register, register)
+  #   else
+  #     conn
+  #     |> put_status(:not_found)
+  #     |> json(%{error: "Product with #{id} not found"})
+  #     |> halt()
+  #   end
+  # end
 
 end
--- phoenix/app/cad_products_phoenix/lib/cad_products_phoenix_web/services/product.ex
+++ phoenix/app/cad_products_phoenix/lib/cad_products_phoenix_web/services/product.ex
@@ -5,21 +5,14 @@ defmodule CadProductsPhoenixWeb.Services.Product do
   alias CadProductsPhoenix.ProductIndex
 
   def fetch_all() do
-   Cache.get("products")
-    case Cache.get("products") do
-      {:ok, _} = result ->
-        result
-      _ ->
-        register = Management.list_register()
-        Cache.set("products", register)
-        {:ok, register}
-      end
+    register = Management.list_register()
+    {:ok, register}
   end
 
   def create(%{"product" => product}) when is_map(product) do
     case Management.create_register(product) do
       {:ok, _} = result ->
-        Cache.delete("products")
+        Cache.set(result.id, result)
         ProductIndex.index_product(result)
         result
       error -> error
@@ -28,10 +21,20 @@ defmodule CadProductsPhoenixWeb.Services.Product do
 
   def create(_params), do: {:error, %{code: 422, message: "Unable to create the product, check if you are passing the json correctly"}}
 
-  def update(product, %{"product" => register_params} ) do
+  def show(id) do
+    {:ok, product} = get_cache(id)
+    product
+  end
+
+  def show(), do: {:error, %{code: 422, message: "Unable to find the product, check if you are passing the id correctly"}}
+
+  def update(id, %{"product" => register_params} ) do
+    {:ok, product} = get_cache(id)
+
     case Management.update_register(product, register_params) do
       {:ok, _} = update_product ->
-        Cache.delete("products")
+        Cache.delete(id)
+        Cache.set(id, update_product)
         ProductIndex.index_product(update_product)
         update_product
       error -> error
@@ -40,12 +43,28 @@ defmodule CadProductsPhoenixWeb.Services.Product do
 
   def update(_product, _params), do: {:error, %{code: 422, message: "Unable to update the product, check if it exists or if you are passing the json correctly"}}
 
-  def delete(product) do
+  def delete(id) do
+    {:ok, product} = get_cache(id)
     Management.delete_register(product)
-    Cache.delete("products")
+    Cache.delete(id)
     ProductIndex.delete_index_product(product)
   end
 
-  def delete(_product), do: {:error, %{code: 422, message: "Unable to delete the product, check if it exists"}}
+  def delete(), do: {:error, %{code: 422, message: "Unable to delete the product, check if it exists"}}
+
+  def get_cache(id) do
+    case Cache.get(id) do
+      {:ok, _} = result ->
+        result
+      _ ->
+        register = Management.get_register(id)
+        if register do
+          Cache.set(id, register)
+          {:ok, register}
+        else
+          {:error, %{code: 422, message: "Product with #{id} not found"}}
+        end
+    end
+  end
 
 end
