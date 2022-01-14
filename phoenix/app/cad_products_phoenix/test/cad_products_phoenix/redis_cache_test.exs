4fe1124fb5bc4602c2df379751d5f0d0bd4d66f2
Aquiles Aguiar <67012626+AquilesAguiar@users.noreply.github.com>
2022-01-14 10:12:45 -0300

    Update redis_cache_test.exs

--- phoenix/app/cad_products_phoenix/test/cad_products_phoenix/redis_cache_test.exs
+++ phoenix/app/cad_products_phoenix/test/cad_products_phoenix/redis_cache_test.exs
@@ -9,22 +9,26 @@ defmodule CadProductsPhoenix.RedisCacheTest do
     }
   end
 
-  def bid_redis_test() do
-    {:ok, conn} = Redix.start_link("redis://localhost:6379/3", name: :redix_test)
-    conn
-  end
+  describe "redis cache test" do
 
-  describe "set/1" do
+    def bid_redis_test() do
+      {:ok, conn} = Redix.start_link("redis://localhost:6379/3", name: :redix_test)
+      conn
+    end
+
     test "set a data in cache, if data is valid", %{key: key, value: value} do
       conn = bid_redis_test()
       assert Redix.command(conn, ["SET", key, value]) == {:ok, "OK"}
     end
-  end
 
-  describe "get/1" do
     test "get a data in cache, if key is valid", %{key: key} do
       conn = bid_redis_test()
       assert Redix.command(conn, ["GET", key]) == {:ok, "some_value"}
+    end
+
+    test "get a data in cache, if key is invalid", %{invalid_key: invalid_key} do
+      conn = bid_redis_test()
+      assert Redix.command(conn, ["GET", invalid_key]) == {:ok, nil}
     end
   end
 end
