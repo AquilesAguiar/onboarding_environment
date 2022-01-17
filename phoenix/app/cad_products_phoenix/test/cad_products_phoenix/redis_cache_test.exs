73d6ba812c7eb0345e43260962daa3d8ecf0f246
Aquiles Aguiar <67012626+AquilesAguiar@users.noreply.github.com>
2022-01-17 09:53:53 -0300

    test delete cache

--- phoenix/app/cad_products_phoenix/test/cad_products_phoenix/redis_cache_test.exs
+++ phoenix/app/cad_products_phoenix/test/cad_products_phoenix/redis_cache_test.exs
@@ -4,6 +4,7 @@ defmodule CadProductsPhoenix.RedisCacheTest do
   alias CadProductsPhoenix.Cache
 
   setup do
+    Cache.flush()
     %{
       key: "some_key",
       value: "some_value",
@@ -13,16 +14,25 @@ defmodule CadProductsPhoenix.RedisCacheTest do
 
   describe "redis cache test" do
     test "set a data in cache, if data is valid", %{key: key, value: value} do
-      Cache.flush()
       assert Cache.set(key, value) == {:ok, "OK"}
     end
 
-    test "get a data in cache, if key is valid", %{key: key} do
+    test "get a data in cache, if key is valid", %{key: key, value: value} do
+      Cache.set(key, value)
       assert Cache.get(key) == {:ok, "some_value"}
     end
 
     test "get a data in cache, if key is invalid", %{invalid_key: invalid_key} do
       assert Cache.get(invalid_key) == {:error, "key not found"}
+    end
+
+    test "delete a data in cache, if key is valid", %{key: key, value: value} do
+      Cache.set(key, value)
+      assert Cache.delete(key) == {:ok, 1}
+    end
+
+    test "delete a data in cache, if key is invalid", %{invalid_key: invalid_key} do
+      assert Cache.delete(invalid_key) == {:ok, 0}
     end
   end
 end
