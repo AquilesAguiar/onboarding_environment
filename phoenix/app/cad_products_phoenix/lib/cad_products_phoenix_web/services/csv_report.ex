85c80d1ffa8806388f4a4ee35e60d25db1068fbb
Aquiles Aguiar <67012626+AquilesAguiar@users.noreply.github.com>
2022-02-01 19:19:50 -0300

    remove unnecessary pipes

--- phoenix/app/cad_products_phoenix/lib/cad_products_phoenix_web/services/csv_report.ex
+++ phoenix/app/cad_products_phoenix/lib/cad_products_phoenix_web/services/csv_report.ex
@@ -2,8 +2,8 @@ defmodule CadProductsPhoenix.Services.CsvReport do
   def tocsv(map) do
     file = File.open!("#{get_folder()}report_products.csv", [:write, :utf8])
 
-    CSV.Encoding.Encoder.encode(map, headers: true)
-    |> Enum.each(&IO.write(file, &1))
+    csv_stream = CSV.Encoding.Encoder.encode(map, headers: true)
+    Enum.each(csv_stream, &IO.write(file, &1))
   end
 
   def delete_file(), do: File.rm("#{get_folder()}report_products.csv")
